812d9cfc023988415cc08814294226883ec5fe8e
spuchades95 <spuchades95@gmail.com>
2024-02-14 18:31:56 +0100

    updates

--- app/Http/Resources/V1/DockWorkerResource.php
+++ app/Http/Resources/V1/DockWorkerResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources\V1;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use app\Models\DockWorker;
+use App\Models\User;
 class DockWorkerResource extends JsonResource
 {
     /**
