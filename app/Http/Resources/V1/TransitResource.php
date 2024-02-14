3d02a647a27c6cd1705e8596dab285d0ec682002
Gabriel Lopez <glopez5@iesfuentesanluis.org>
2024-02-14 13:50:08 +0100

    transitresource

--- app/Http/Resources/V1/TransitResource.php
+++ app/Http/Resources/V1/TransitResource.php
@@ -4,16 +4,43 @@ namespace App\Http\Resources\V1;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\Boat;
+use App\Models\Facility;
+use App\Models\Dock;
+use App\Models\Berth;
 
 class TransitResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
+     * @param  Request  $request
      * @return array<string, mixed>
      */
-    public function toArray(Request $request): array
+    public function toArray($request): array
     {
-        return parent::toArray($request);
+        $embarcacion = Boat::find($this->author_id);
+        $instalacion = Facility::find($this->facility_id);
+        $pantalan = Dock::find($this->dock_id);
+        $amarre = Berth::find($this->berth_id);
+
+        $ubicacion = '';
+        if ($instalacion) {
+            $ubicacion .= $instalacion->Ubicacion;
+        }
+        if ($pantalan) {
+            $ubicacion .= ' - ' . $pantalan->Nombre;
+        }
+        if ($amarre) {
+            $ubicacion .= ' - Amarre ' . $amarre->id;
+        }
+
+        return [
+            'FechaEntrada' => $this->FechaEntrada,
+            'FechaSalida' => $this->FechaSalida,
+            'Embarcacion' => $embarcacion ? $embarcacion->Matricula : null,
+            'Ubicacion' => $ubicacion,
+            'Patron' => $embarcacion ? $embarcacion->Titular : null,
+        ];
     }
 }
