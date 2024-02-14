f41140bbab67e965da0ea402b24a87562e4a2414
piterxus <piterxus@gmail.com>
2024-02-14 12:57:09 +0100

    Controlador administrativo

--- app/Http/Controllers/Api/V1/AdministrativeController.php
+++ app/Http/Controllers/Api/V1/AdministrativeController.php
@@ -13,7 +13,7 @@ class AdministrativeController extends Controller
      */
     public function index()
     {
-        //
+        return Administrative::all();
     }
 
     /**
@@ -21,7 +21,9 @@ class AdministrativeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $administrative = Administrative::create($request->all());
+        $administrative->save();
+        return response()->json($administrative, 201);
     }
 
     /**
@@ -29,7 +31,18 @@ class AdministrativeController extends Controller
      */
     public function show(Administrative $administrative)
     {
-        //
+        $administrative = Administrative::find($administrative);
+
+        if ($administrative == null) {
+            return response()->json([
+                'message' => 'No se encuentra el administrativo',
+                'code' => 404
+            ], 404);
+        }
+        return response()->json([
+            'data' => $administrative,
+            'code' => 200
+        ], 200);
     }
 
     /**
@@ -37,14 +50,52 @@ class AdministrativeController extends Controller
      */
     public function update(Request $request, Administrative $administrative)
     {
-        //
+        try {
+            // Verifica si el administrativo existe
+            $administrative = Administrative::find($administrative);
+            if ($administrative == null) {
+                return response()->json([
+                    'message' => 'No se encuentra el administrativo',
+                    'code' => 404
+                ], 404);
+            }
+            // Actualiza el administrativo
+            $administrative->update($request->all());
+            return response()->json([
+                'data' => $administrative,
+                'code' => 200
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Error al actualizar el administrativo',
+                'code' => 500
+            ], 500);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Administrative $administrative)
+    public function destroy($id)
     {
-        //
+        try {
+            $administrative = Administrative::find($id);
+            if ($administrative == null) {
+                return response()->json([
+                    'message' => 'No se encuentra el administrativo',
+                    'code' => 404
+                ], 404);
+            }
+            $administrative->delete();
+            return response()->json([
+                'message' => 'Administrativo eliminado',
+                'code' => 200
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Error al eliminar el administrativo',
+                'code' => 500
+            ], 500);
+        }
     }
 }
