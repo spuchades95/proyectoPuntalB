32889077965fd4d061931f3e5887820e3a7bc7b4
ddavidsalinas <davideosalinosa@gmail.com>
2024-02-14 13:26:26 +0100

    w

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('v1/usuario', App\Http\Controllers\Api\V1\UserController::class);
 Route::apiResource('v1/rol', App\Http\Controllers\Api\V1\RoleController::class);
 Route::apiResource('v1/transito', App\Http\Controllers\Api\V1\TransitController::class);
+Route::apiResource('v1/administrativo', App\Http\Controllers\Api\V1\AdministrativeController::class);
+Route::apiResource('v1/plazaBase', App\Http\Controllers\Api\V1\BaseBerthController::class);
+Route::apiResource('v1/guardiaCivil', App\Http\Controllers\Api\V1\CivilGuardController::class);
+Route::apiResource('v1/concesionario', App\Http\Controllers\Api\V1\ConcessionaireController::class);
+Route::apiResource('v1/tripulante', App\Http\Controllers\Api\V1\CrewController::class);
+Route::apiResource('v1/plaza', App\Http\Controllers\Api\V1\DockController::class);
+Route::apiResource('v1/guardamuelles', App\Http\Controllers\Api\V1\DockWorkerController::class);
+Route::apiResource('v1/instalacion', App\Http\Controllers\Api\V1\FacilityController::class);
+Route::apiResource('v1/incidencia', App\Http\Controllers\Api\V1\IncidentController::class);
+
+
+
 
