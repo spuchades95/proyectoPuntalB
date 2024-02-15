b07325f98694cac1279239589c104beae572516e
piterxus <piterxus@gmail.com>
2024-02-15 11:53:51 +0100

    BERTH FIX

--- app/Models/Berth.php
+++ app/Models/Berth.php
@@ -9,7 +9,7 @@ class Berth extends Model
 {
     use HasFactory;
     use SoftDeletes;
-    protected $with = ['administrativoamare'];
+  
     protected $fillable = [
         'Estado',
         'TipoPlaza',
