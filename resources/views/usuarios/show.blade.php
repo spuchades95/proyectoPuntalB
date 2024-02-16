8bdaf5b03fe9868c798b85e641661e822514264a
Piterxus <piterxus@gmail.com>
2024-02-16 15:55:42 +0100

    User controller ok

---
+++ resources/views/usuarios/show.blade.php
@@ -0,0 +1,44 @@
+<table>
+    <thead>
+        <tr>
+            <th>Nombre Completo</th>
+            <th>Habilitado</th>
+            <th>Nombre usuario</th>
+            <th>Instalación</th>
+            <th>DNI</th>
+            <th>Telefono</th>
+            <th>Dirección</th>
+            <th>Imagen</th>
+            <th>Descripción</th>
+            <th>Rol</th>
+            <th>Causa</th>
+            <th>Email</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>{{ $usuario->NombreCompleto }}</td>
+            <td>{{ $usuario->Habilitado }}</td>
+            <td>{{ $usuario->NombreUsuario }}</td>
+            <td>{{ $usuario->Instalacion_id }}</td>
+            <td>{{ $usuario->DNI }}</td>
+            <td>{{ $usuario->Telefono }}</td>
+            <td>{{ $usuario->Direccion }}</td>
+            <td>{{ $usuario->Imagen }}</td>
+            <td>{{ $usuario->Descripcion }}</td>
+            <td>{{ $usuario->Rol }}</td>
+            <td>{{ $usuario->Causa }}</td>
+            <td>{{ $usuario->Email }}</td>
+            <td><a href="{{ route('usuarios.show', $usuario->id) }}">Ver</a></td>
+            <td><a href="{{ route('usuarios.edit', $usuario->id) }}">Editar</a></td>
+            <td>
+                <form action="{{ route('usuarios.destroy', $usuario->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit">Borrar</button>
+                </form>
+            </td>
+        </tr>
+    </tbody>
+</table>
