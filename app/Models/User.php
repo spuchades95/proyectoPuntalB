bd4e3a257bfa907d79c51712a910679f0e3bf932
spuchades95 <spuchades95@gmail.com>
2024-02-17 19:24:30 +0100

    cambios

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\softDeletes;
 
-class User extends Authenticatable  implements JWTSubject
+class User extends Authenticatable  
 {
     use HasApiTokens, HasFactory, Notifiable;
     use SoftDeletes;
