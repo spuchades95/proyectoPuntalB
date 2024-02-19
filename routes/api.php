78f4698a8a878686654c69bc74da4a18695c2d09
piterxus <piterxus@gmail.com>
2024-02-19 12:17:00 +0100

    Rutas jwt

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    // Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    // Route::get('/user-profile', [AuthController::class, 'userProfile']);    
 });
-
 Route::apiResource('v1/usuario', App\Http\Controllers\Api\V1\UserController::class);
 Route::apiResource('v1/rol', App\Http\Controllers\Api\V1\RoleController::class);
 Route::apiResource('v1/transito', App\Http\Controllers\Api\V1\TransitController::class);
