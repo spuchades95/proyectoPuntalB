6593c8642fff6092eff2739b891dd8edb60cc3c8
Gabriel Lopez <glopez5@iesfuentesanluis.org>
2024-02-21 08:41:18 +0100

    dock edit

--- resources/views/instalaciones/edit.blade.php
+++ resources/views/instalaciones/edit.blade.php
@@ -1,29 +1,204 @@
-<form action="{{route('instalaciones.update', ['instalacione'=>$instalacion->id])}}" method="POST">
-    @csrf
-    @method('PUT')
-    <div class="form-group">
-        <label for="Ubicacion">Ubicación</label>
-        <input type="text" name="Ubicacion" id="Ubicacion" class="form-control" value="{{$instalacion->Ubicacion}}" required>
+@extends('layouts.plantilla')
+
+@section('content')
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<div class="formularioRoles">
+    <div class="formHeader">
+        <h5>EDITAR INSTALACION {{ $instalacion->Ubicacion }} </h5>
     </div>
-    <div class="form-group">
-        <label for="Dimensiones">Dimensiones</label>
-        <input type="text" name="Dimensiones" id="Dimensiones" class="form-control" value="{{$instalacion->Dimensiones}}" required>
+    <form class="form-container" method="POST" action="{{ route('instalaciones.update', ['instalacione' => $instalacion->id]) }}">
+        @csrf
+        @method('PUT')
+        <div class="mb-3 d-flex">
+            <label for="Ubicacion" class="form-label">Ubicación:</label>
+            <input name="Ubicacion" type="text" class="form-control mt-4" placeholder="Ubicación" value="{{ $instalacion->Ubicacion }}" required />
+        </div>
+        <div class="mb-3 d-flex">
+            <label for="Dimensiones" class="form-label">Dimensiones:</label>
+            <input name="Dimensiones" type="text" class="form-control mt-4" placeholder="Dimensiones" value="{{ $instalacion->Dimensiones }}" required />
+        </div>
+        <div class="mb-3 d-flex">
+            <label for="Descripcion" class="form-label">Descripción:</label>
+            <textarea name="Descripcion" class="form-control mt-4" required>{{ $instalacion->Descripcion }}</textarea>
+        </div>
+        <div class="mb-3 d-flex">
+            <label for="Estado" class="form-label">Estado:</label>
+            <input name="Estado" type="text" class="form-control mt-4" placeholder="Estado" value="{{ $instalacion->Estado }}" required />
+        </div>
+        <div class="mb-3 d-flex">
+            <label for="FechaCreacion" class="form-label">Fecha de creación:</label>
+            <input name="FechaCreacion" type="date" class="form-control mt-4" value="{{ $instalacion->FechaCreacion }}" required />
+        </div>
+        <div class="mb-3 d-flex" >
+            <label for="Causa" class="form-label">Causa:</label>
+            <input name="Causa" type="text" class="form-control mt-4" placeholder="Causa" value="{{ $instalacion->Causa }}">
+        </div>
+<div style='text-align:right' class='mt-4'>
+        <button type="button" class="btn btnCancelar" data-toggle="modal" data-target="#myModal">
+    ELIMINAR
+</button>
+        <button type="submit" class="btn btnAdd">EDITAR</button>
+        </div>
+    </form>
     </div>
-    <div class="form-group">
-        <label for="Descripcion">Descripción</label>
-        <input type="text" name="Descripcion" id="Descripcion" class="form-control" value="{{$instalacion->Descripcion}}" required>
+
+
+<!-- Botón de eliminación fuera del formulario -->
+<!-- <button type="button" class="btn btnCancelar" data-toggle="modal" data-target="#myModal">
+    ELIMINAR
+</button> -->
+
+<!-- Modal para confirmar la eliminación -->
+<div class="modal" id="myModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <!-- Cabecera del Modal -->
+            <div class="modal-header">
+                <h4 class="modal-title">BAJA DE INSTALACION</h4>
+            </div>
+            <!-- Cuerpo del Modal -->
+            <div class="modal-body">
+                <div class="mb-3 d-flex">
+                    <label for="UbicacionInstalacion">Ubicación de instalación:</label>
+                    <input name="UbicacionInstalacion" type="text" class="form-control mt-4" placeholder="Ubicación del instalacion " value="{{ $instalacion->Ubicacion }}" readonly />
+                </div>
+                <div class="mb-3 d-flex">
+                    <label for="Causa" class="form-label">Causa de la baja:</label>
+                    <textarea name="Causa" class="form-control mt-4"></textarea>
+                </div>
+            </div>
+            <!-- Pie del Modal -->
+            <div class="modal-footer">
+                <button type="button" class="btn btnCancelar" data-dismiss="modal">CANCELAR</button>
+                <form id="delete-form" action="{{ route('instalaciones.destroy', ['instalacione' => $instalacion->id]) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btnAdd">ELIMINAR</button>
+                </form>
+            </div>
+        </div>
     </div>
-    <div class="form-group">
-        <label for="Estado">Estado</label>
-        <input type="text" name="Estado" id="Estado" class="form-control" value="{{$instalacion->Estado}}" required>
-    </div>
-    <div class="form-group">
-        <label for="FechaCreacion">Fecha de creación</label>
-        <input type="date" name="FechaCreacion" id="FechaCreacion" class="form-control" value="{{$instalacion->FechaCreacion}}" required>
-    </div>
-    <div class="form-group">
-        <label for="Causa">Causa</label>
-        <input type="text" name="Causa" id="Causa" class="form-control" value="{{$instalacion->Causa}}">
-    </div>
-    <button type="submit" class="btn btn-primary">Actualizar</button>
-</form>
+</div>
+
+<style>
+
+.btnAdd {
+    background-color: #3f2d85!important;
+    color: #f5f6fd!important;
+    margin-left: 10px;
+
+
+}
+
+.btn{
+
+    text-align: center;
+    -webkit-text-stroke: 1.5px ;
+font-family: Questrial;
+font-size: 16px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+
+
+}
+
+.btnCancelar {
+    text-decoration: none;
+    background-color:#ffc745!important;
+    color: #7a2e0d!important;
+   
+}
+
+
+.formHeader {
+        padding: 6px;
+        font-weight: bold;
+        font-family: 'Questrial', sans-serif;
+        color: #ffffff;
+        background-color: #1d2834;
+        border-radius: 5px 5px 0px 0px;
+    }
+
+    .formularioRoles {
+        padding: 50px;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+    }
+
+    form {
+        background-color: #FFFFFF;
+        padding: 15px;
+        border-radius: 0px 0px 5px 5px;
+        box-shadow: 10px 5px 10px rgba(0, 0, 0, 0.5);
+    }
+
+    .chec div {
+        margin-bottom: 5px;
+    }
+
+    input[type="text"],
+    textarea,
+    input[type="checkbox"] {
+        box-sizing: border-box;
+        border: 1px solid #ccc;
+        padding: 3px;
+        font-size: 14px;
+        line-height: 1.5;
+        height: 28px;
+        width: calc(100% - 75px);
+    }
+
+    .label-checkbox {
+        font-weight: bold;
+        display: inline-block;
+        width: 200px;
+        margin-right: 10px;
+    }
+
+    label {
+        width: 200px;
+        font-weight: bold;
+    }
+
+
+    .modal-content {
+        background-color: #F7F7F7;
+        border: 1px solid #888;
+        border-radius: 10px;
+    }
+
+    .modal-header {
+        background-color: #1D2834;
+        color: #fff;
+        border-bottom: 1px solid #007bff;
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+    }
+
+
+    .modal-footer {
+        border-top: none;
+    }
+
+    .modal-footer .btn {
+        border-radius: 5px;
+    }
+
+    label {
+        width: 200px;
+        font-weight: bold;
+        text-align: left;
+    }
+
+</style>
+
+@endsection
--- resources/views/instalaciones/show.blade.php
+++ resources/views/instalaciones/show.blade.php
@@ -1,37 +1,132 @@
-<table>
-    <tr>
-        <th>Ubicación</th>
-        <td>{{ $instalacion->Ubicacion }}</td>
-    </tr>
-    <tr>
-        <th>Dimensiones</th>
-        <td>{{ $instalacion->Dimensiones }}</td>
-    </tr>
-    <tr>
-        <th>Descripción</th>
-        <td>{{ $instalacion->Descripcion }}</td>
-    </tr>
-    <tr>
-        <th>Estado</th>
-        <td>{{ $instalacion->Estado }}</td>
-    </tr>
-    <tr>
-        <th>Fecha de creación</th>
-        <td>{{ $instalacion->FechaCreacion }}</td>
-    </tr>
-    <tr>
-        <th>Causa</th>
-        <td>{{ $instalacion->Causa }}</td>
-    </tr>
-    <tr>
-        <th>Acciones</th>
-        <td>
-            <a href="{{ route('instalaciones.edit', $instalacion->id) }}">Editar</a>
-            <form action="{{ route('instalaciones.destroy', $instalacion->id) }}" method="POST">
-                @csrf
-                @method('DELETE')
-                <button type="submit">Eliminar</button>
-            </form>
-        </td>
-    </tr>
-</table>
+@extends('layouts.plantilla')
+
+@section('content')
+<link rel="stylesheet" type="text/css" href="{{ asset('styles.css') }}">
+
+<div class="formularioRoles">
+    <div class="formHeader">
+        <h5>VISTA INSTALACIÓN {{ $instalacion->Ubicacion }} </h5>
+    </div>
+    <form class="form-container">
+        <div class="mb-3 d-flex">
+            <label for="Ubicacion" class="form-label">Ubicación:</label>
+            <input name="Ubicacion" type="text" class="form-control mt-4" value="{{ $instalacion->Ubicacion }}" readonly />
+        </div>
+        <div class="mb-3 d-flex">
+            <label for="Dimensiones" class="form-label">Dimensiones:</label>
+            <input name="Dimensiones" type="text" class="form-control mt-4" value="{{ $instalacion->Dimensiones }}" readonly />
+        </div>
+        <div class="mb-3 d-flex">
+            <label for="Descripcion" class="form-label">Descripción:</label>
+            <textarea name="Descripcion" class="form-control mt-4" readonly>{{ $instalacion->Descripcion }}</textarea>
+        </div>
+        <div class="mb-3 d-flex">
+            <label for="Estado" class="form-label">Estado:</label>
+            <input name="Estado" type="text" class="form-control mt-4" value="{{ $instalacion->Estado }}" readonly />
+        </div>
+        <div class="mb-3 d-flex">
+            <label for="FechaCreacion" class="form-label">Fecha de creación:</label>
+            <input name="FechaCreacion" type="text" class="form-control mt-4" value="{{ $instalacion->FechaCreacion }}" readonly />
+        </div>
+        <div class="mb-3 d-flex">
+            <label for="Causa" class="form-label">Causa:</label>
+            <input name="Causa" type="text" class="form-control mt-4" value="{{ $instalacion->Causa }}" readonly />
+        </div>
+        <div class="mb-3">
+            <div style='text-align:right' class='mt-4'>
+                <!-- <a href="{{ route('instalaciones.edit', $instalacion->id) }}" class="btn btnVista">Editar</a>
+                <form action="{{ route('instalaciones.destroy', $instalacion->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btnDelete">Eliminar</button>
+                </form> -->
+
+                <a href="{{ route('instalaciones.index') }}" class="btn btnDelete">VOLVER</a>
+
+                <a href="{{ route('instalaciones.edit', $instalacion->id) }}" class="btn btnVista">EDITAR</a>
+            </div>
+        </div>
+    </form>
+</div>
+
+<style>
+  .btnVista {
+    background-color: #add8e6 !important;
+    color: #162f3b!important;
+    margin-left: 10px;
+
+}
+
+.btn{
+
+    text-align: center;
+    -webkit-text-stroke: 1.5px ;
+font-family: Questrial;
+font-size: 16px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+
+}
+
+.btnDelete {
+    text-decoration: none;
+    background-color:#ffd700!important;
+    color: #442604!important;
+}
+
+
+    .formHeader {
+        padding: 6px;
+        font-weight: bold;
+        font-family: 'Questrial', sans-serif;
+        color: #ffffff;
+        background-color: #1d2834;
+        border-radius: 5px 5px 0px 0px;
+    }
+
+    .formularioRoles {
+        padding: 50px;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+    }
+
+    form {
+        background-color: #FFFFFF;
+        padding: 15px;
+        border-radius: 0px 0px 5px 5px;
+        box-shadow: 10px 5px 10px rgba(0, 0, 0, 0.5);
+    }
+
+    .chec div {
+        margin-bottom: 5px;
+    }
+
+    input[type="text"],
+    textarea,
+    input[type="checkbox"] {
+        box-sizing: border-box;
+        border: 1px solid #ccc;
+        padding: 3px;
+        font-size: 14px;
+        line-height: 1.5;
+        height: 28px;
+        width: calc(100% - 75px);
+    }
+
+    .label-checkbox {
+        font-weight: bold;
+        display: inline-block;
+        width: 200px;
+        margin-right: 10px;
+    }
+
+    label {
+        width: 200px;
+        font-weight: bold;
+    }
+</style>
+
+@endsection
