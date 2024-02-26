6becb8a027940a8baf40916583bbed23f23df7c0
piterxus <piterxus@gmail.com>
2024-02-26 13:18:44 +0100

    Cards panel

--- resources/views/panel/index.blade.php
+++ resources/views/panel/index.blade.php
@@ -6,15 +6,36 @@
 <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.7.0.js"></script>
 <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
-<link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <h1>PANEL</h1>
 <p>Usuarios registrados: {{ $totalUsers }}</p>
 <p>Roles registrados: {{ $totalroles }}</p>
 <p>Instalaciones registradas: {{ $totalfacilities }}</p>
-<div style="width: 80%; margin: auto;">
+<div class="cardsContainerUp d-flex">
+<div class="card">
         <canvas id="barChart"></canvas>
+        
     </div>
+    <div class="card">
+        <canvas id="barChart2"></canvas>
+    </div>
+    <div class="card">
+        <canvas id="barChart3"></canvas>
+    </div>
+</div>
+<div class="cardsContainerDown d-flex">
+<div class="card">
+        <canvas id="barChart4"></canvas>
+        
+    </div>
+    <div class="card">
+        <canvas id="barChart5"></canvas>
+    </div>
+    <div class="card">
+        <canvas id="barChart6"></canvas>
+    </div>
+</div>
 
 <script>
         var ctx = document.getElementById('barChart').getContext('2d');
@@ -38,6 +59,122 @@
                 }
             }
         });
+        var ctx2 = document.getElementById('barChart2').getContext('2d');
+        var myChart2 = new Chart(ctx2, {
+            type: 'doughnut',
+            data: {
+                labels: @json($data['labels']),
+                datasets: [{
+                    label: 'Data',
+                    data: @json($data['data']),
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+        var ctx3 = document.getElementById('barChart3').getContext('2d');
+        var myChart3 = new Chart(ctx3, {
+            type: 'bar',
+            data: {
+                labels: @json($data['labels']),
+                datasets: [{
+                    label: 'Data',
+                    data: @json($data['data']),
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+        var ctx4 = document.getElementById('barChart4').getContext('2d');
+        var myChart4 = new Chart(ctx4, {
+            type: 'bar',
+            data: {
+                labels: @json($data['labels']),
+                datasets: [{
+                    label: 'Data',
+                    data: @json($data['data']),
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+        var ctx5 = document.getElementById('barChart5').getContext('2d');
+        var myChart5 = new Chart(ctx5, {
+            type: 'bar',
+            data: {
+                labels: @json($data['labels']),
+                datasets: [{
+                    label: 'Data',
+                    data: @json($data['data']),
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+        var ctx6 = document.getElementById('barChart6').getContext('2d');
+        var myChart6 = new Chart(ctx6, {
+            type: 'bar',
+            data: {
+                labels: @json($data['labels']),
+                datasets: [{
+                    label: 'Data',
+                    data: @json($data['data']),
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
     </script>
+    <style>
+        .card {
+            width: 20%;
+            margin: auto;
+            border: 2px solid black;
+        }
+        .cardDown{
+            margin-top: 20px;
+        }
+    </style>
 
 @endsection
